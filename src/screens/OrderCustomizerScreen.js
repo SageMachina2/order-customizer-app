061e1efbccfb9bdb10aeb1513ec2b7b133d45cef
SageMachina2 <nguyen.a225214@stu.sanjac.edu>
2026-09-14 23:15:46 +0000

    Calculate order total

--- src/screens/OrderCustomizerScreen.js
+++ src/screens/OrderCustomizerScreen.js
@@ -46,8 +46,17 @@ export default function OrderCustomizerScreen() {
 );
   }
 
+  
+
+  
+
   const total = useMemo(() => {
     // TODO 7: Calculate the total in Step 7.
+    const addOnTotal = menuItem.addOns
+  .filter((item) => selectedAddOns.includes(item.id))
+  .reduce((sum, item) => sum + item.price, 0);
+
+return (menuItem.basePrice + addOnTotal) * quantity;
     return menuItem.basePrice;
   }, [quantity, selectedAddOns]);
 
